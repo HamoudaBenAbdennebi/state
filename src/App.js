925fca497f91464f1739594b460325dd78863b0f
HamoudaBenAbdennebi <hamoudabenabdennebi77@gmail.com>
2020-10-25 14:21:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    fullName:"Hamouda ben abdennebi",
+    bio:"i am tunisian and i live in la marsa",
+    imgSrc:"./profile.jpg",
+    profession:"web student at GOMYCODE",
+    show: false,
+    time:0
+  }
+  componentDidMount=() =>{
+    setInterval(()=>{
+      let time = new Date()
+      time = time.getHours()+':'+time.getMinutes()
+      this.setState({time:time})
+    },1000)
+    
+  }
+  handleClick=()=>this.setState({show: !this.state.show})
+  render(){
+    return(
+      <>
+
+      {
+        this.state.show && (
+          <>
+            <h1>fullName : {this.state.fullName}</h1>
+            <p>bio : {this.state.bio}</p>
+            <img src={this.state.imgSrc} alt={"hello"}/>
+            <p>profession : {this.state.profession}</p>
+            <p>{this.state.time}</p>
+          </>
+        )
+
+      }
+        <button onClick={this.handleClick}> ClickMe</button>
+      </>
+    )
+  }
 }
 
 export default App;
